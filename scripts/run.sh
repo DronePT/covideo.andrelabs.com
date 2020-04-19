f4537aba1a6dcc02ba9dbca7c37d9c8ca71efa84
André Mota <dronept@gmail.com>
2020-04-19 20:40:14 +0100

    :whale: Remove www. subdomain from run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 docker container rm -f covideo.andrelabs.com
 docker container run -d --name covideo.andrelabs.com \
         -e "NODE_ENV=production" \
-        -e "VIRTUAL_HOST=covideo.andrelabs.com,www.covideo.andrelabs.com" \
-        -e "LETSENCRYPT_HOST=covideo.andrelabs.com,www.covideo.andrelabs.com" \
+        -e "VIRTUAL_HOST=covideo.andrelabs.com" \
+        -e "LETSENCRYPT_HOST=covideo.andrelabs.com" \
         -e "LETSENCRYPT_EMAIL=dronept@gmail.com" \
         covideo.andrelabs.com
