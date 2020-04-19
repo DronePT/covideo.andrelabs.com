09a40ebfde0c23af6a35d4824eacc52cd25c98bc
André Mota <dronept@gmail.com>
2020-04-19 20:36:38 +0100

    :whale: Remove port publish on run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 docker container rm -f covideo.andrelabs.com
 docker container run -d --name covideo.andrelabs.com \
-        -p 8080:80 \
         -e "NODE_ENV=production" \
         -e "VIRTUAL_HOST=covideo.andrelabs.com,www.covideo.andrelabs.com" \
         -e "LETSENCRYPT_HOST=covideo.andrelabs.com,www.covideo.andrelabs.com" \
